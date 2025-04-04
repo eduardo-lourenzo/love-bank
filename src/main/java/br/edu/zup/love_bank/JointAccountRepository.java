d9a45eee73b94dfdf49bff61f4f6360a0e0be635
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-04-04 14:39:05 -0300

    feat: create JoinAccountRepository class

---
+++ src/main/java/br/edu/zup/love_bank/JointAccountRepository.java
@@ -0,0 +1,6 @@
+package br.edu.zup.love_bank;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JointAccountRepository extends JpaRepository<JointAccountEntity, Long> {
+}
