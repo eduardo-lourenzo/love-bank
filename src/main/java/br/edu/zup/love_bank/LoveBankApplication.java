7a709a486ed3fcd216021857e566796fa528d163
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-04-04 14:14:33 -0300

    build: spring initializr

---
+++ src/main/java/br/edu/zup/love_bank/LoveBankApplication.java
@@ -0,0 +1,13 @@
+package br.edu.zup.love_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoveBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/zup/love_bank/LoveBankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.zup.love_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoveBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
